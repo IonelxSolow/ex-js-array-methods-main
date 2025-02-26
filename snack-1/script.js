e9cd5d872e5531faf78b117bc10c867f63d3eff6
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-26 14:58:41 +0100

    add loop for into script.js with forEach method and loop for traditional in foltder snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// forEach method
+const printName = names.forEach(name => console.log(name))
+
+
+// loop for traditional
+for (let i = 0; i < names.length; i++){
+    logName = names[i];
+    console.log(logName)
+}
