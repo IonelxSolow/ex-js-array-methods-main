929123a1d9c41a16c9b546006ff33e5da8e3bb8f
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-26 15:35:12 +0100

    add for loop in script.js with .map method and traditional for loop in snack3 folder to create new array with elements incremented by 1

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,15 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+// .map method
+const newArr = numbers.map(number => number +1);
+console.log(newArr);
+
+
+// for loop traditional
+const newArr2 = [];
+for (let i = 0; i < numbers.length; i++){
+    newArr2.push(numbers[i] + 1);  
+};console.log(newArr2);
