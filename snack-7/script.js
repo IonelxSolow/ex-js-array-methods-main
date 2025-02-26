883ba440b73d57a1614eb7aa8c94bc27e6d85147
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-26 16:42:57 +0100

    add for loop in script.js with .find method and traditional for loop in snack7 folder to find student with id 2

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,18 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const student2 = students.find(student => student.id === 2);
+console.log(student2)
+
+
+
+
+let studente2 = [];
+for (let i = 0; i < students.length; i++){
+  if(students[i].id === 2){
+    studente2 = students[i];
+    break;
+  }
+}
+console.log(studente2)
