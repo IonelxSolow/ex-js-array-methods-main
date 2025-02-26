d8cb24f8f02a43ee77212456806601e96ad41970
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-26 17:03:03 +0100

    add for loop in script.js with .find method and traditional for loop in snack8 folder to find student class

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,20 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studente = students.find(student => student.name === 'Marco Lanci');
+/* console.log(studente) */
+
+const classStudente = studente ? studente.class : 'Studente non trovato';
+
+console.log(classStudente)
+
+
+
+let studentClass = 'Studente non trovato';
+for (let i = 0; i < students.length; i++){
+  if (students[i].name === 'Marco Lanci'){
+    studentClass = students[i].class;
+  }
+}
+console.log(studentClass)
