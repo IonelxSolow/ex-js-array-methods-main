20871978ed00a49b6d9707e44eba4d810f8c0926
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-26 16:31:36 +0100

    add for loop in script.js with .filter method and traditional for loop in snack6 folder to filter the shorter zuchins and longer zuchins into new array

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,28 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = zucchine.filter(zucchina => zucchina.length >= 15);
+
+const zucchineCorte = zucchine.filter(zucchina => zucchina.length < 15); 
+
+console.log('Zucchine più lunghe di 10cm: ', zucchineLunghe);
+console.log('Zucchine più corte di 10cm: ', zucchineCorte);
+
+
+// for tradizionale con if >=lunghe e else < corte
+
+const longZuchins = [];
+const shortZuchins = [];
+
+for (let i = 0; i < zucchine.length; i++) {
+  if (zucchine[i].length >= 10){
+    longZuchins.push(zucchine[i]);
+  }
+  if (zucchine[i].length < 10){
+    shortZuchins.push(zucchine[i]);
+  }
+};
+console.log('Zucchine più lunghe di 10cm: ',longZuchins);
+console.log('Zucchine più corte di 10cm: ',shortZuchins);
+
